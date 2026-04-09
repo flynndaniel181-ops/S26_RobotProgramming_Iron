e4ff56fc08d038168866b1415f0f5d6d56cf1a6a
Ian Chuang <ian.chuang27@gmail.com>
2026-04-09 11:05:03 +0800

    Add Lab 6 folder for chuang

---
+++ Lab6/chuang/roscourse_sim_ws/src/my_robot/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'my_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[(os.path.join('share', package_name), glob('urdf/*')),
+(os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+(os.path.join('share', package_name), glob('rviz/*')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yahboom',
+    maintainer_email='ian.chuang27@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
